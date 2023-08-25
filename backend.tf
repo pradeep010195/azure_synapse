ce5e8508faa112fddcb5099cc42a659d634ee04d
pradeep <Pradeep.Kumar@adastragrpde.onmicrosoft.com>
2023-08-25 22:36:59 +0200

    Added backend file

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "mohamed-rg"
+    storage_account_name = "pvlab53f35fadls"
+    container_name       = "terraform-statefile"
+    key                  = "terraformstate.tf"
+  }
+}
+
