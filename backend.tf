168bfb9a931c3d6cff0a7fa526b9cc3be0e46894
pradeep <Pradeep.Kumar@adastragrpde.onmicrosoft.com>
2023-10-30 12:42:14 +0100

    fdvars

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "mohamed-rg"
-    storage_account_name = "pvlab53f35fadls"
+    resource_group_name  = "pradeep-dev"
+    storage_account_name = "storageaccountemailatch"
     container_name       = "terraform-state1"
     key                  = "terraformstate-sub1.tf"
   }
